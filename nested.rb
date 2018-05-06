67b8353e3a7cea8e6237d3591d687c819d8a4d19
George  Pianka <gpianka1@gmail.com>
2018-05-06 03:52:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,10 +15,10 @@ def hopper
           :languages => ["C"]
         }
      }
-binding.pry
+
 
 end
-
+binding.pry
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
